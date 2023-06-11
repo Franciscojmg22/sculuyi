054358275de61d8263dfdbe6b094c1fa2bc3774c
JashiRL <j.robleslecona@ugto.mx>
2023-06-11 12:35:03 -0600

    en proceso de creación de la paginad e cartas de contacto

--- contacto.php
+++ contacto.php
@@ -1,11 +1,46 @@
 <?php require_once('./components/header.php')?>
 
-<link rel="stylesheet" href="./css/contact.css">
+<link rel="stylesheet" href="./css/contactcard.css">
 
     <main>
 
         <div id="mainContainer" class="mainContainer">
-            CONTACTOssss
+            <div class="contenedor"> 
+                <!-- Carta de cointacto de Paco-->
+                <div class="card">
+                    <figure>
+                        <img src="./visuales/pic_paco.jpg" class="img-paco" >
+                    </figure>
+                    <div class="contenido">
+                        <h3>Francisco de Jesús Martínez González </h3>
+                        <p></p>
+                        <a href=""></a>
+                    </div>
+                </div>
+                    <!-- Carta de contacto de Cabal-->
+                <div class="card">
+                    <figure>
+                        <img src="./visuales/pic_cabal.jpg" class="img-cabal" >
+                    </figure>
+                    <div class="contenido">
+                        <h3>José Edaurdo Cabal Páramo  </h3>
+                        <p></p>
+                        <a href=""></a>
+                    </div>
+                </div>
+
+                    <!--Carta de contacto de Jashi -->
+                <div class="card">
+                    <figure>
+                        <img src="./visuales/pic_jashi.jpg" class="img-jashi" >
+                    </figure>
+                    <div class="contenido">
+                        <h3>Jashiel Robles Lecona </h3>
+                        <p></p>
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
