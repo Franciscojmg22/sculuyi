5f7c4724793eac3b4a33059724ef263331120e8f
Franciscojmg22 <fdj.martinez@outlook.es>
2023-06-11 23:57:24 -0600

    final

--- maestros.php
+++ maestros.php
@@ -1,88 +1,43 @@
-<?php session_start()?>
-<?php require_once('./components/header.php')?>
-   
- <link rel="stylesheet" href="./css/maestros.css">
-    <main>
+<?php
+session_start();
+require 'connection.php';
+
+if (isset($_SESSION['user_id'])) {
+    $query = "SELECT * FROM maestros";
+    $registro = $conn->prepare($query);
+    $registro->execute();
+}
+
+
+?>
+<?php require_once('./components/header.php') ?>
+
+<link rel="stylesheet" href="./css/maestros.css">
+<main>
     <div id="mainContainer" class="mainContainer">
-                    <h1 class="tittle">Conoce a nuestros maestros
-                    <i class="bi bi-laptop-fill"></i>
-                    </h1>
-            <div class="contenedor"> 
+        <h1 class="tittle">Conoce a nuestros maestros
+            <i class="bi bi-laptop-fill"></i>
+        </h1>
+        <div class="contenedor">
 
-                    <!-- carta bde contacto de Paco -->
-                            <div class="card">
-                    <figure>
-                        <img src="./visuales/John.jpg" class="img-paco" >
-                    </figure>
-                    <div class="contenido">
-                        <h3>Johnny Johnson </h3>
-                        <!-- <p>¿Qué tal?, soy johnny uun profesor de desarrollo web enfocado a VUE3js
-                            tengo 27 años y me entusiasma estar enseñando en esta plataforma
-                        </p>
-                        <a href="" class="face">
-                        <i class="bi bi-facebook"></i>
-                        </a> -->
-                        
-                    </div>
-                </div>
-
-                    <!-- Carta de contacto de Cabal-->
-                    
-                <div class="card">
-                    <figure>
-                        <img src="./visuales/peter.png" class="img-cabal" >
-                    </figure>
-                    <div class="contenido">
-                        <h3>Peter B. Parker</h3>
-                       
-                    </div>
-                </div>
-
-                    <!--Carta de contacto de Jashi -->
+            <!-- carta bde contacto de Paco -->
+            <?php while (($resultado = $registro->fetch(PDO::FETCH_ASSOC))): ?>
 
                 <div class="card">
                     <figure>
-                        <img src="./visuales/obi-wan.jpeg" class="img-jashi" >
+                        <img src="./visuales/John.jpg" class="img-paco">
                     </figure>
                     <div class="contenido">
-                        <h3>Obi-wan Kenobi</h3>
-                    <!--    <p>Soy un jovén de 20 años al cual le encanta la programación,
-                        procuro siempre estar al dia con los nuevos frameworks y
-                        si quieres algún consejo sobre frameworks no dudes en contactarte conmigo
-                        </p>
-                        <a href="https://www.facebook.com/Jashi.RL/" class="face">
-                        <i class="bi bi-facebook"></i>
-                        </a>    
-                        
-                        <a href="https://www.youtube.com/watch?v=vFIyR9YbME0" class="face">
-                            <i class="bi bi-youtube" class="you"></i>
-                        </a>  -->
+                        <h3><?php echo $resultado['nombre']?> </h3>
+                        <p><?php echo $resultado['descripcion']?></p>
                     </div>
                 </div>
+            <?php endwhile; ?>
 
-                <div class="card">
-                    <figure>
-                        <img src="./visuales/uwu.png" class="img-jashi" >
-                    </figure>
-                    <div class="contenido">
-                        <h3>Sensei uwu </h3>
-                    </div>
-                </div>
-
-
-                <div class="card">
-                    <figure>
-                        <img src="./visuales/marco-polo.jpg" class="img-jashi" >
-                    </figure>
-                    <div class="contenido">
-                        <h3>Marco Aurelio</h3>
-                    </div>
-                </div>
-
-                
-            </div>
+            <!-- Carta de contacto de Cabal-->
         </div>
-    </main>
+    </div>
+</main>
 
-    
-<?php require_once('./components/footer.php')?>
+
+<?php require_once('./components/footer.php') ?>
