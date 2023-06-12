7ae15a39fb139d5d92ffec10cc7191619d5ab757
unknown <je.cabalparamo@ugto.mx>
2023-06-11 21:27:12 -0600

    Ahora si funciona login y registro tambien puse algo de logout, registro de nuevo curso 2.9 :)

--- db/sculuyi.sql
+++ db/sculuyi.sql
@@ -42,7 +42,7 @@ CREATE TABLE `alumnos` (
 
 CREATE TABLE `curso` (
   `id` int(11) NOT NULL,
-  `nom_curs` varchar(60) NOT NULL,
+  `nom_curs` varchar(255) NOT NULL,
   `descripcion` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
