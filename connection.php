5c5b621abd824b6763d17b90e170adcb7f0f5fe2
Franciscojmg22 <fdj.martinez@outlook.es>
2023-06-11 19:25:55 -0600

    registro de alumnos a cursos

--- connection.php
+++ connection.php
@@ -1,14 +1,10 @@
 <!--Creo que esta linea es importante para la conexion a la base de datos, si ven algun problema lo comentan porfavor-->
 <?php 
     $server = 'localhost';
-    $username = '524465';
-    $password = 'sculuyi2';
-    $database = '524465';
-
-    $server = 'localhost';
     $username = 'root';
-    $password = '';
+    $password = 'root';
     $database = 'sculuyi';
+    
     try {
         $conn = new PDO("mysql:host=$server; dbname=$database;", $username, $password);
     } catch (PDOException $error) {
